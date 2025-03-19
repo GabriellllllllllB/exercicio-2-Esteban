1c7cfcadccac18954b8774512b77d249e237e2d0
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-18 23:10:10 -0300

    Add files via upload

---
+++ exercicio6/Program.cs
@@ -0,0 +1,27 @@
+﻿int n;
+int i;
+int idade;
+int maisve = 0;
+string nomemaisve = "";
+string nome;
+string[]nomeidade;
+
+Console.WriteLine("escreva quantos numeros seram lidos:");
+n = int.Parse(Console.ReadLine());
+
+int[] vetor = new int[n];
+
+for (i = 0; i < n; i++)
+{
+    Console.WriteLine("escreva o nome e idade");
+    nomeidade = Console.ReadLine().Split();
+    nome = nomeidade[0];
+    idade = int.Parse(nomeidade[1]);
+
+    if (idade > maisve)
+    {
+        maisve = idade;
+        nomemaisve = nome;
+    }
+}
+Console.WriteLine($"o mais velho: {nomemaisve}");
---
+++ exercicio7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+double media = 0;
+int i;
+string nome;
+double nota1;
+double nota2;
+
+Console.WriteLine("escreva quantos alunos seram lidos:");
+int n = int.Parse(Console.ReadLine());
+
+List<string> alunosap = new List<string>();
+for (i = 0; i < n; i++)
+{
+    Console.WriteLine("escreva o nome e a nota");
+    string[] nomeNota = Console.ReadLine().Split();
+
+    nome = nomeNota[0];
+    nota1 = double.Parse(nomeNota[1]);
+    nota2 = double.Parse(nomeNota[2]);
+
+    media = (nota1 + nota2) / 2;
+
+
+    if (media >= 6)
+    {
+        alunosap.Add(nome);
+    }
+
+}
+Console.WriteLine($"alunos aprovados:");
+foreach (var aluno in alunosap)
+{
+    Console.WriteLine(aluno);
+}
